c42eed5f5f7645d50c991a3b88e5dc4b8f3b879d
Arjun01coder <arjun251898@gmail.com>
2026-03-22 12:37:54 +0530

    correctly unwrap ApiResponse data for login and account APIs

--- src/main/java/com/trustbank/app/controller/BankController.java
+++ src/main/java/com/trustbank/app/controller/BankController.java
@@ -14,7 +14,18 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:5500", "http://127.0.0.1:5500"})
+@CrossOrigin(origins = {
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://localhost:5500",
+    "http://localhost:5501",
+    "http://localhost:8080",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:5500",
+    "http://127.0.0.1:5501",
+    "http://127.0.0.1:8080"
+})
 public class BankController {
 
     private static final Logger logger = LoggerFactory.getLogger(BankController.class);
